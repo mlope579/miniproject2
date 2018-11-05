a0ec7213568a996a05f906ef9c9f88c6f6b538ac
mlope579 <44369570+mlope579@users.noreply.github.com>
2018-11-04 22:30:38 -0500

    Add files via upload

---
+++ miniproject2.py
@@ -0,0 +1,39 @@
+adjective = raw_input("Enter an adjective, please: ")
+
+print("Flip-flops are a staple of any " + adjective + " summer wardrobe.")
+
+article_of_clothing = raw_input("Enter an article of clothing, please: ")
+
+print("My gym locker always stinks because I'm always leaving my dirty " + article_of_clothing + " in there.")
+
+verb1 = raw_input("Enter a verb, please: ")
+
+print("This hot water makes me want to " + verb1 + " on the beach all day long.")
+
+adjective1 = raw_input("Enter an adjective, please: ")
+
+print("Life is too " + adjective1 + " not to wear a bikini on spring break!.")
+
+type_of_liquid = raw_input("Enter a type of liquid, please: ")
+
+print("Spring break poolparties are just like the ones you used to have when you were a kid... except with way more " + type_of_liquid + "!")
+
+plural_noun1 = raw_input("Enter a plural noun, please: ")
+
+print("Nobody loves spring flowers more than me, except maybe bumble-  " + plural_noun1 + ".")
+
+number1 = raw_input("Enter a number, please: ")
+
+print("One margarita, two margarita, " + number1 + " margaritas, floor!")
+
+noun2 = raw_input("Enter a noun, please: ")
+
+print("BBQ at my house! Everyone's invated, and it's Bring Your Own " + noun2 + "!")
+
+type_of_food = raw_input("Enter a type of food, please: ")
+
+print("My Favorite way to flirt is to ask if I can have a bite of your " + type_of_food + "!")
+
+plural_noun2 = raw_input("Enter a plural noun, please: ")
+
+print("My heart is filled with love and " + plural_noun2)
